07f2273eae6f684254f3573989a8a8b06899275c
David Zherdenovsky <dzherdenovsky@gmail.com>
2024-01-28 17:29:09 +0200

    added router-dom and link to Find a beer page in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import logo from '../assets/logo.png';
-import './navbar.css'
-const Navbar = ()=>{
-
-    return(
-        
-        <nav className='navigation'>
-            <div className='main-navigation'>
-            <img src={logo} alt="Company Logo" className='logo' />
-            <ul className='navigation-list'>
-                <li>Home</li>
-                <li>Find a beer</li>
-                <li>Contact Us</li>
-            </ul>
-            </div>
-        </nav>
-    );
-}
+import React from "react";
+import logo from "../assets/logo.png";
+import "./navbar.css";
+import { Outlet, Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navigation">
+      <div className="main-navigation">
+        <img src={logo} alt="Company Logo" className="logo" />
+        <ul className="navigation-list">
+          <li>
+            <Link to="/" className="remove-link-colors">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="findabeer" className="remove-link-colors">
+              Find a Beer
+            </Link>
+          </li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Home from "./pages/Home/Home";
 import reportWebVitals from "./reportWebVitals";
+import FindABeer from "./pages/FindABeer/FindABeer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+export default function App() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="home" element={<Home />}></Route>
+          <Route path="findabeer" element={<FindABeer />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
